4d2870c7d8261968b44c1375daf0b0546dc053da
FairyPenguin <autorun81@gmail.com>
2024-04-23 06:51:29 +0200

    Fix CSS quesation and Fix the answers allowing any text as an input

---
+++ RelatedProducts/RelatedProducts.tsx
@@ -0,0 +1,20 @@
+// ======/ Imports /====== 
+
+// CSS-Module File Import
+import styles  from "./RelatedProducts.module.css";
+
+// Main Component Exported Function
+
+export default function RelatedProducts() {
+
+    return (
+
+        <>
+            <h1>RelatedProducts Component, Generated via React-Outil</h1>
+        </>
+
+    )
+
+}
+
+
